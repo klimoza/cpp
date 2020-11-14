13a0a68c1e3b0ff09a6796753ca3817b202ffabf
klimoza <i220303@mail.ru>
2020-11-14 13:46:10 +0300

    Fix some bugs

--- SPbU_F20-S21/lab_02/include/str.h
+++ SPbU_F20-S21/lab_02/include/str.h
@@ -1,9 +1,9 @@
 #ifndef H_STR
 #define H_STR
 
-char* my_strcpy(char* s1, char* s2);
-char* my_strcat(char* s1, char* s2);
-int my_strcmp(char* s1, char* s2);
+char* my_strcpy(char* restrict s1, const char* restrict s2);
+char* my_strcat(char* restrict s1, const char* restrict s2);
+int my_strcmp(const char* s1, const char* s2);
 int my_strlen(char* s);
 
 #endif
